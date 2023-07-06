892344f44e7f3a2cb925a644ce41c56b964d6473
xMurzik <murzik6667@gmail.com>
2023-07-06 11:55:37 +0300

    route ready + route protection

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainScreen from '../../pages/main-screen/main-screen';
-import { offersCount } from '../../consts';
+import FavoritesScreen from '../../pages/favorites-screen/favourites-screen';
+import LoginScreen from '../../pages/login-screen/login-screen';
+import OfferScreen from '../../pages/offer-screen/offer-screen';
+import NotFound from '../not-found/not-found';
+import RouteProtection from '../route-protection/route-protection';
+import { OFFERS_COUNT, PATHS_NAMES, AUTH_STATUS } from '../../consts';
 
 const App: React.FC = () => (
   <BrowserRouter>
-    <MainScreen countOffers={offersCount} />
+    <Routes>
+      <Route path={PATHS_NAMES.Main}>
+        <Route index element={<MainScreen countOffers={OFFERS_COUNT} />} />
+        <Route
+          path={PATHS_NAMES.Favorite}
+          element={
+            <RouteProtection authStatus={AUTH_STATUS.NoAuth}>
+              <FavoritesScreen />
+            </RouteProtection>
+          }
+        />
+        <Route path={PATHS_NAMES.Login} element={<LoginScreen />} />
+        <Route path={`${PATHS_NAMES.Offer}/:id`} element={<OfferScreen />} />
+        <Route path={PATHS_NAMES.NotFound} element={<NotFound />} />
+      </Route>
+    </Routes>
   </BrowserRouter>
 );
 
--- src/consts.ts
+++ src/consts.ts
@@ -1,1 +1,15 @@
-export const offersCount = 10;
+export const OFFERS_COUNT = 10;
+
+export const PATHS_NAMES = {
+  Main: '/',
+  Favorite: 'FAVOURITE',
+  Login: 'LOGIN',
+  Offer: 'OFFER',
+  NotFound: '*',
+} as const;
+
+export const AUTH_STATUS = {
+  Auth: 'AUTH',
+  NoAuth: 'NO_AUTH',
+  Unknown: 'UNKNOWN',
+} as const;
