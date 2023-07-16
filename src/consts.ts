27fb11d5879a5f87eeb983c53c0d6507801ea96d
xMurzik <murzik6667@gmail.com>
2023-07-16 23:11:40 +0300

    changed cosnt url base name

--- src/consts.ts
+++ src/consts.ts
@@ -1,12 +1,12 @@
 export const OFFERS_COUNT = 10;
 export const COUNT_STARS = [1, 2, 3, 4, 5];
 
-const MARKER_BASE_URL =
+const URL_MARKER_BASE =
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/';
 
-export const URL_MARKER_DEFAULT = `${MARKER_BASE_URL}pin.svg`;
+export const URL_MARKER_DEFAULT = `${URL_MARKER_BASE}pin.svg`;
 
-export const URL_MARKER_CURRENT = `${MARKER_BASE_URL}main-pin.svg`;
+export const URL_MARKER_CURRENT = `${URL_MARKER_BASE}main-pin.svg`;
 
 export const TILE_LAYER_ULR =
   'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png';
