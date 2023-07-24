db1a598ecfec8c9a1c5562907b2626fc8aa8ef13
xMurzik <murzik6667@gmail.com>
2023-07-24 22:56:31 +0300

    fix:edited header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -17,6 +17,51 @@ const Header: React.FC = () => {
     dispatch(logoutAction());
   };
 
+  let navbar;
+
+  if (isLogged === AuthorizationStatus.NoAuth) {
+    navbar = (
+      <nav className="header__nav">
+        <ul className="header__nav-list">
+          <li className="header__nav-item user">
+            <span className="header__nav-link header__nav-link--profile">
+              <div className="header__avatar-wrapper user__avatar-wrapper"></div>
+              <span
+                onClick={() => navigate(Path.Login)}
+                className="header__login"
+              >
+                Sign in
+              </span>
+            </span>
+          </li>
+        </ul>
+      </nav>
+    );
+  } else {
+    navbar = (
+      <nav className="header__nav">
+        <ul className="header__nav-list">
+          <li className="header__nav-item user">
+            <a className="header__nav-link header__nav-link--profile" href="#">
+              <div className="header__avatar-wrapper user__avatar-wrapper"></div>
+              <span className="header__user-name user__name">
+                Oliver.conner@gmail.com
+              </span>
+              <span className="header__favorite-count">3</span>
+            </a>
+          </li>
+          <li className="header__nav-item">
+            <span className="header__nav-link">
+              <span onClick={onClickButtonLogout} className="header__signout">
+                Sign out
+              </span>
+            </span>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+
   return (
     <header className="header">
       <div className="container">
@@ -33,51 +78,7 @@ const Header: React.FC = () => {
               />
             </Link>
           </div>
-          {!isNavToRender &&
-            (isLogged === AuthorizationStatus.NoAuth ? (
-              <nav className="header__nav">
-                <ul className="header__nav-list">
-                  <li className="header__nav-item user">
-                    <span className="header__nav-link header__nav-link--profile">
-                      <div className="header__avatar-wrapper user__avatar-wrapper"></div>
-                      <span
-                        onClick={() => navigate(Path.Login)}
-                        className="header__login"
-                      >
-                        Sign in
-                      </span>
-                    </span>
-                  </li>
-                </ul>
-              </nav>
-            ) : (
-              <nav className="header__nav">
-                <ul className="header__nav-list">
-                  <li className="header__nav-item user">
-                    <a
-                      className="header__nav-link header__nav-link--profile"
-                      href="#"
-                    >
-                      <div className="header__avatar-wrapper user__avatar-wrapper"></div>
-                      <span className="header__user-name user__name">
-                        Oliver.conner@gmail.com
-                      </span>
-                      <span className="header__favorite-count">3</span>
-                    </a>
-                  </li>
-                  <li className="header__nav-item">
-                    <span className="header__nav-link">
-                      <span
-                        onClick={onClickButtonLogout}
-                        className="header__signout"
-                      >
-                        Sign out
-                      </span>
-                    </span>
-                  </li>
-                </ul>
-              </nav>
-            ))}
+          {!isNavToRender && navbar}
         </div>
       </div>
     </header>
