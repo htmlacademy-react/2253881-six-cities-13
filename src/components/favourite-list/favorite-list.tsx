fa5b152e578331bb59e93c0574c09b07a0bd8fde
xMurzik <murzik6667@gmail.com>
2023-08-02 00:52:43 +0300

    littel fix for loader

--- src/components/favourite-list/favorite-list.tsx
+++ src/components/favourite-list/favorite-list.tsx
@@ -1,17 +1,21 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import { RotatingLines } from 'react-loader-spinner';
+import EmptyFavoritesList from '../empty-favorites-list/empty-favorites-list';
+import FavouriteItem from '../favourite-item/favourite-item';
 import { getToken } from '../../services/token';
 import { BASE_BACKEND_URL, APIRoute, Path } from '../../consts';
-import FavouriteItem from '../favourite-item/favourite-item';
-import EmptyFavoritesList from '../empty-favorites-list/empty-favorites-list';
 import { IOffer } from '../../types/offers';
+import styles from './favourite-list.module.css';
 
 const FavoriteList: React.FC = () => {
   const [favOffers, setFavOffers] = useState<Array<IOffer>>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const downloadFavOffers = useCallback(async () => {
     const token = getToken();
+    setIsLoading(true);
     const res = await axios.get<Array<IOffer>>(
       `${BASE_BACKEND_URL}${APIRoute.Favorite}`,
       {
@@ -21,17 +25,33 @@ const FavoriteList: React.FC = () => {
     const { data } = res;
 
     setFavOffers(data);
+    setIsLoading(false);
   }, []);
 
   useEffect(() => {
     downloadFavOffers();
   }, [downloadFavOffers]);
 
+  if (isLoading) {
+    return (
+      <div className={styles.containerForLoader}>
+        <RotatingLines
+          strokeColor="grey"
+          strokeWidth="3"
+          animationDuration="0.75"
+          width="150"
+          visible
+        />
+      </div>
+    );
+  }
+
   const cityList = Array.from(new Set(favOffers.map((el) => el.city.name)));
 
-  if (favOffers.length === 0) {
+  if (favOffers.length === 0 && !isLoading) {
     return <EmptyFavoritesList />;
   }
+
   return (
     <section className="favorites">
       <h1 className="favorites__title">Saved listing</h1>
